5fbf1689209857d004c936a898b8db6a26fde036
rakibulhasan933 <rakibulmd933@gmail.com>
2024-01-19 23:16:03 +0600

    [ADD] shadcn ui added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-2xl">Hello World</h1>
+      <Button variant="outline">Get Start</Button>
     </div>
   );
 }
