13baeec3abb7d32531830a4a1b1665db907b238f
rakibulhasan933 <rakibulmd933@gmail.com>
2024-01-23 01:05:53 +0600

    [ADD] prarall route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import TanStackProvider from "@/components/TanStackProvider";
 import { ContextProvider } from "@/components/ContextProvider";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
