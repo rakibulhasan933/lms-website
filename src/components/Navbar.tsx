2d3c1a17a6a20178075eb15d69290303ed00a490
rakibulhasan933 <rakibulmd933@gmail.com>
2024-01-23 19:31:10 +0600

    [ADD] clear

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,10 @@
 import Link from 'next/link'
 import React from 'react'
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
 	Sheet,
 	SheetClose,
 	SheetContent,
-	SheetDescription,
-	SheetFooter,
 	SheetHeader,
 	SheetTitle,
 	SheetTrigger,
